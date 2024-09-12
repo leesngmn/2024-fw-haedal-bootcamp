6179613754eda14f7fb7e73b45b4d9ba99dc2ef5
Leeseungmin <lsm12372@naver.com>
2024-09-12 19:59:25 +0900

    hw02

---
+++ a-b.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void){
+    int a,b;
+    0 < a, b < 10;
+    scanf("%d %d", &a, &b);
+    printf("%d\n", a-b);
+    return 0;
+}
---
+++ plusminus.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void){
+    int A, B;
+    1 <= A, B <= 10,000;
+    scanf("%d %d", &A, &B);
+    printf("%d\n", A+B);
+    printf("%d\n", A-B);
+    printf("%d\n", A*B);
+    printf("%d\n", A/B);
+    printf("%d\n", A%B);
+    return 0;
+}
+
